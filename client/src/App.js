6257791a1a8fe693d8450aaaad0c8cf27c28eda6
Sonz <sonyarangraj@msn.com>
2021-08-17 04:32:14 +0200

    changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient } from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//apollo
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-boost';
+
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem('id_token');
+    // uri: 'http://localhost:3001/graphql'
 
     operation.setContext({
       headers: {
