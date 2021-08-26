ef2bd4bff68e8f35eb63bb875a64ed7f41ef34ab
Sonz <sonyarangraj@msn.com>
2021-08-26 05:25:16 +0200

    defined SAVE_BOOKS

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -6,6 +6,7 @@ import { useMutation} from '@apollo/react-hooks';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
