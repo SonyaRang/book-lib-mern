a8845ecfcdf8efe5dafa76ee79916fac9e0847ba
Sonz <sonyarangraj@msn.com>
2021-08-26 04:38:05 +0200

    changes on mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import Auth from '../utils/auth';
-import { SAVE_BOOK } from '../utils/mutations';
+import { SAVE_BOOK } from '../utils/mutation';
 import { useMutation} from '@apollo/react-hooks';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
